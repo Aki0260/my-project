9f49e174cf2dbc05dcb0b857028458465626568e
chiaki <0mj326621w6613@gmail.com>
2026-08-15 23:36:06 +0900

    ルーターを作りっました

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,10 +5,18 @@ import ProjectCreateView from '@/views/ProjectCreateView.vue'
 import ProjectDetailView from '@/views/ProjectDetailView.vue'
 import ProjectEditView from '@/views/ProjectEditView.vue'
 
+import TodoListView from '@/views/TodoListView.vue'
+import TodoCreateView from '@/views/TodoCreateView.vue'
+import TodoDetailView from '@/views/TodoDetailView.vue'
+import TodoEditView from '@/views/TodoEditView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 
   routes: [
+    // =========================
+    // Project
+    // =========================
     {
       path: '/',
       component: ProjectListView,
@@ -24,6 +32,26 @@ const router = createRouter({
     {
       path: '/projects/:id/edit',
       component: ProjectEditView,
+    },
+
+    // =========================
+    // Todo
+    // =========================
+    {
+      path: '/projects/:id/todos',
+      component: TodoListView,
+    },
+    {
+      path: '/projects/:id/todos/new',
+      component: TodoCreateView,
+    },
+    {
+      path: '/todos/:id',
+      component: TodoDetailView,
+    },
+    {
+      path: '/todos/:id/edit',
+      component: TodoEditView,
     },
   ],
 })
